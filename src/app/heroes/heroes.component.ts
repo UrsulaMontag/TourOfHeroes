ffa4b9b23a8c22daae6606985cced0165c468552
U_Montag <montagu666@gmail.com>
2022-07-21 20:09:26 +0200

    Get browser data for detail view

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,8 +10,6 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
 
-  selectedHero?: Hero;
-    
   heroes: Hero[] = [];
 
   constructor(
@@ -22,11 +20,6 @@ export class HeroesComponent implements OnInit {
     /**call getHeroes() here and not in constructor (constructor just for minimal initialization such as wiring constructor parameters to properties)!
      * If called inside ngOnInit, Angular call this at an appropriate time after constructing a HeroesComponent instance!*/
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 
   getHeroes(): void {
